4532772b18a68bc9ddff2a22552ade3f848f9710
Miguel Valenzuela <migpics@gmail.com>
2015-12-09 15:01:51 -0800

    Changed Percentages for slider bars

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ var context = a_canvas.getContext("2d");
 
 var bar1, slider1;
 var bar2, slider2;
-var set_perc1=.50;
+var set_perc1=.20;
 var set_perc2=.50;
-var set_perc3=.50;
+var set_perc3=.40;
 var set_perc4=.30;
-var set_perc5=.50;
-var set_perc6=.85;
-var set_perc7=.50;
+var set_perc5=.30;
+var set_perc6=.55;
+var set_perc7=.60;
 
 canvas_width=400;
 canvas_height=200;
